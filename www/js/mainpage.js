bdbf130c6f291b7efa5c831428d1b85226a02109
PimDamen <pim.damen@wearewuunder.com>
2022-11-01 12:45:14 +0100

    Jquery boeken.. not working
    not working setup jquery need troubleshooting.

--- www/js/mainpage.js
+++ www/js/mainpage.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
 
 $(".no-JS").remove();   // removes warning div for the site if JS is disabled. 
 
+$.getJSON("../json/boeken.json", function(data){
+    var boeken_markup = '';
+    $.each(data, function(key, value){
+        boeken_markup += '<<div id="boekenCardHolder"><div class="card">';        
+        boeken_markup += '<img src="'+ value.image +'" alt="">'; 
+        boeken_markup += '<p class="boekPrijs">'+ value.prijs +'</p>';
+        boeken_markup += '<div class="cardContent">';		
+        boeken_markup += '<p>'+ value.description +'</p><br><hr>';
+        boeken_markup += '<h2>'+ value.title +'</h2>';
+        boeken_markup += '<button>'+ value.buttonName +'</button>';
+        boeken_markup += '</div></div></div>';    
+    });
+    $('.homeBoeken').append(boeken_markup);
+});
 });
