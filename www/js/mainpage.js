dec70e70a414230b910c8de501e9889b4bbf4ac6
PimDamen <pim.damen@wearewuunder.com>
2022-11-01 15:43:28 +0100

    artikellen generator done
    artikellen generator done

--- www/js/mainpage.js
+++ www/js/mainpage.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 
 $(".no-JS").remove();   // removes warning div for the site if JS is disabled. 
 
+
+// boeken card creator. 
 $.getJSON("./json/boeken.json", function(data){
     var boeken_markup = '';
     console.log(data);
@@ -21,4 +23,23 @@ $.getJSON("./json/boeken.json", function(data){
     });
     $('.homeBoeken').append(boeken_markup);
 });
+
+// Artikellen Card ceator
+$.getJSON("./json/artikellen.json", function(data){
+    var artikellen_markup = '';
+    console.log(data);
+    $.each(data, function(key, value){
+        console.log(value);
+        artikellen_markup += '<div class="artikelCard"><div class="cardSpacer"><div class="cardContent">';        
+        artikellen_markup += '<h2>'+ value.title +'</h2>';
+        artikellen_markup += '<p>'+ value.tekstPreview +'</p>';		
+        artikellen_markup += '<img src="'+ value.image +'" alt="'+ value.title +'">';
+        artikellen_markup += ' <button>Volledig Artikel</button>';
+        artikellen_markup += '</div></div></div>';    
+    });
+    $('.artikelCardHolder').append(artikellen_markup);
 });
+
+}); // end/closing of script
+ 
+
