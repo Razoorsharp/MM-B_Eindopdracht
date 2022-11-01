70c311731e5c0614de27ee5c1ac1bc5ecccf4834
PimDamen <pim.damen@wearewuunder.com>
2022-11-01 16:27:51 +0100

    recepten
    recepten

--- www/js/mainpage.js
+++ www/js/mainpage.js
@@ -8,37 +8,54 @@ $(".no-JS").remove();   // removes warning div for the site if JS is disabled.
 
 // boeken card creator. 
 $.getJSON("./json/boeken.json", function(data){
-    var boeken_markup = '';
+    var boekenMarkup = '';
     console.log(data);
     $.each(data, function(key, value){
         console.log(value);
-        boeken_markup += '<<div id="boekenCardHolder"><div class="card">';        
-        boeken_markup += '<img src="'+ value.image +'" alt="">'; 
-        boeken_markup += '<p class="boekPrijs">'+ value.prijs +'</p>';
-        boeken_markup += '<div class="cardContent">';		
-        boeken_markup += '<p>'+ value.description +'</p><br><hr>';
-        boeken_markup += '<h2>'+ value.title +'</h2>';
-        boeken_markup += '<button>'+ value.buttonName +'</button>';
-        boeken_markup += '</div></div></div>';    
+        boekenMarkup += '<<div id="boekenCardHolder"><div class="card">';        
+        boekenMarkup += '<img src="'+ value.image +'" alt="">'; 
+        boekenMarkup += '<p class="boekPrijs">'+ value.prijs +'</p>';
+        boekenMarkup += '<div class="cardContent">';		
+        boekenMarkup += '<p>'+ value.description +'</p><br><hr>';
+        boekenMarkup += '<h2>'+ value.title +'</h2>';
+        boekenMarkup += '<button>'+ value.buttonName +'</button>';
+        boekenMarkup += '</div></div></div>';    
     });
     $('.homeBoeken').append(boeken_markup);
 });
 
 // Artikellen Card ceator
 $.getJSON("./json/artikellen.json", function(data){
-    var artikellen_markup = '';
+    var artikellenMarkup = '';
     console.log(data);
     $.each(data, function(key, value){
         console.log(value);
-        artikellen_markup += '<div class="artikelCard"><div class="cardSpacer"><div class="cardContent">';        
-        artikellen_markup += '<h2>'+ value.title +'</h2>';
-        artikellen_markup += '<p>'+ value.tekstPreview +'</p>';		
-        artikellen_markup += '<img src="'+ value.image +'" alt="'+ value.title +'">';
-        artikellen_markup += ' <button>Volledig Artikel</button>';
-        artikellen_markup += '</div></div></div>';    
+        artikellenMarkup += '<div class="artikelCard"><div class="cardSpacer"><div class="cardContent">';        
+        artikellenMarkup += '<h2>'+ value.title +'</h2>';
+        artikellenMarkup += '<p>'+ value.tekstPreview +'</p>';		
+        artikellenMarkup += '<img src="'+ value.image +'" alt="'+ value.title +'">';
+        artikellenMarkup += ' <button>Volledig Artikel</button>';
+        artikellenMarkup += '</div></div></div>';    
     });
     $('.artikelCardHolder').append(artikellen_markup);
 });
+
+// recepten Card ceator @note: nog afmaken
+$.getJSON("./json/recepten.json", function(data){
+    var recepteMarkup = '';
+    console.log(data);
+    $.each(data, function(key, value){
+        console.log(value);
+        receptenMarkup += '<div class="artikelCard"><div class="cardSpacer"><div class="cardContent">';        
+        receptenMarkup += '<h2>'+ value.title +'</h2>';
+        receptenMarkup += '<p>'+ value.tekstPreview +'</p>';		
+        receptenMarkup += '<img src="'+ value.image +'" alt="'+ value.title +'">';
+        receptenMarkup += ' <button>Volledig Artikel</button>';
+        receptenMarkup += '</div></div></div>';    
+    });
+    $('.artikelCardHolder').append(artikellen_markup);
+});
+
 
 }); // end/closing of script
  
