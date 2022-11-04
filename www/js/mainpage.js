76f5a0ed8b210e6f59b11179d29012542ab5bcef
PimDamen <pim.damen@wearewuunder.com>
2022-11-04 11:11:48 +0100

    Recept icons automaticly
    correct and automaticly made icons.

--- www/js/mainpage.js
+++ www/js/mainpage.js
@@ -5,7 +5,13 @@ $(document).ready(function(){
 
 $(".no-JS").remove();   // removes warning div for the site if JS is disabled. 
 
-
+// Page resolution RWD variable defining. 
+var pageResForIcons = '';
+if (!window.matchMedia('(min-width: 768px)').matches) {
+	pageResForIcons = '32px';
+} else if(window.matchMedia('(min-width: 768px)').matches){
+	pageResForIcons = '64px';
+}
 
 // boeken card creator. 
 $.getJSON("./json/boeken.json", function(data){
@@ -49,11 +55,14 @@ $.getJSON("./json/recepten.json", function(data){
         console.log(value);
         receptenMarkup += ' <div class="receptCard"><div class="cardSpacer"><div class="receptHead">';        
         receptenMarkup += '<h2>'+ value.title +'</h2>';
+        // defineer variabelen voor de juiste iconen gebaseerd op de Json
+        let soortGerei = value.benodigdheden;
         let soortGerecht = value.hoofdIngredient;
-        receptenMarkup += '<ul><li class="receptHoofdIngr">'+ receptIconPlacement(soortGerecht); +'</li><li class="receptGerei">.</li><li class="receptTijd">.</li></ul>'; // Maak icons aan en run daarna en functie
-        
+        let bereidTijd = value.tijd;
+        // receptenMarkup += '<ul>><li class="receptGerei">.</li><li class="receptHoofdIngr">'+ receptIconPlacement(soortGerecht); +'</li><li class="receptTijd">.</li></ul>'; // Maak icons aan en run daarna en functie
+         receptenMarkup += receptIconPlacement(soortGerei,soortGerecht,bereidTijd);
         // icons function
-        receptenMarkup += receptIconPlacement(soortGerecht);
+      
         receptenMarkup += '</div>';
         receptenMarkup += '<div class="receptText">';
         receptenMarkup += '<ul>';
@@ -68,14 +77,32 @@ $.getJSON("./json/recepten.json", function(data){
     });
     $('.receptCardHolder').append(receptenMarkup);
 });
+        // receptIconPLacement function. i = Sooftgerecht in de recepten constructor. en plaatst het benodigde icoon per recept card er in. 
+ function receptIconPlacement(nrGerei,nrGerecht,nrTijd){ 
+     iconGerei = '<ul>><li class="receptGerei">.</li>';
+     iconGerecht = '</li><li class="receptHoofdIngr"><img src="images/icons/'+ pageResForIcons +'/61.png"></li>';
+     iconTijd = '<li class="receptTijd">.</li></ul>';
+     // icon gerei defineren. 
+         iconGerei = '<ul>><li class="receptGerei"><img src="images/icons/'+ pageResForIcons +'/'+ nrGerei +'.png"></li>';
+    // icon hoofd ingredient defineren
+        iconGerecht = '</li><li class="receptHoofdIngr"><img src="images/icons/'+ pageResForIcons +'/'+ nrGerecht +'.png"></li>';
+    // icon en tekst tijd 
+    iconTijd = '<li class="receptTime'+ nrTijd +'"><img src="images/icons/'+ pageResForIcons +'/163.png"></li></ul>'
 
- function receptIconPlacement(i){
-     
-     console.log(i);
-    if(i == "gevogelte"){       
-        let html = '<img src="images/icons/32px/61.png">'
-        return html;
-    }
+     return iconGerei+iconGerecht+iconTijd;
+
+
+//      console.log(i);
+//         if(i == "gevogelte"){       
+//     let html = '<img src="images/icons/'+ pageResForIcons +'/61.png">'
+//         return html;
+//     }else if(i == "vlees"){               
+//             let html = '<img src="images/icons/'+ pageResForIcons +'/129.png">'
+//             return html;
+//     }else if(i == "vis"){               
+//         let html = '<img src="images/icons/'+ pageResForIcons +'/127.png">'
+//         return html;
+// }
      
     //  if (this.value.ingredienten)
  }
