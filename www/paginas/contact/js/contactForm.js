b2a33dc35c8021064083534bebbeaf6150ee8c18
PimDamen <pim.damen@wearewuunder.com>
2022-12-09 16:00:21 +0100

    fixed contact form
    fixed contact form

--- www/paginas/contact/js/contactForm.js
+++ www/paginas/contact/js/contactForm.js
@@ -11,34 +11,37 @@ var contactSubmit = d.getElementById("contactSubmit")
 
 
 
-function valideerForm (welkveld){
-    var userInput = welkveld.value;
+function valideerForm (welkVeld){
+    var userInput = welkVeld.value;
     var valid = false;
    
-    if (welkveld.id == naam){
+    if (welkVeld.id == "naam"){
         var pattern = /^[a-z0-9\s]{4,50}$/i;
         valid = pattern.test(naam.value); // test waarde van het veld, dit is standaard functie van Js. 
         console.log("ik check naam");
-    } else if (welkveld.id == email){
+    } else if (welkVeld.id == "email"){
         var pattern = /^[a-z0-9-_]{2,50}@[a-z]{2,50}.[a-z]{2,50}$/i;
         valid = pattern.test(email.value); // test waarde van het veld, dit is standaard functie van Js. 
         console.log("ik ben email");
         
     }
-    createFeedback(welkveld, valid);
+    createFeedback(welkVeld, valid);
 
     return valid;
 }
 
-function createFeedback(welkVeld, status, melding = 'Controleer door veld'){
+function createFeedback(welkVeld, status){
+    let veld = d.getElementById("error"+ welkVeld.name);
     if(status){
         welkVeld.style.backgroundColor = 'rgba(0,255,0,0.2)';
         welkVeld.style.borderColor = 'rgb(0,255,0)';
+        veld.style.display = 'none';
       
     }else{
         welkVeld.style.backgroundColor = 'rgba(255,0,0,0.2)';
         welkVeld.style.borderColor = 'rgb(255,0,0)';
-        
+       console.log("error"+ welkVeld.name);
+        veld.style.display = 'block';        
         }
 }
 
