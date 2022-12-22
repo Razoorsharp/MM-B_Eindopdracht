bcb4b83cb2236b8d50f6db4a5841b1289e6ca9f0
PimDamen <pim.damen@wearewuunder.com>
2022-12-22 15:50:45 +0100

    trail error comments
    trail and errors.
    Maak de markup in de Done
    Comments added

--- www/paginas/recepten/js/show.js
+++ www/paginas/recepten/js/show.js
@@ -8,50 +8,45 @@ $(document).ready(function () {
     // -----------------------------
     // Determine receptId
     // ------------------------------
-    var receptId = GetParameterValues('receptId');
-    // alert("Hello your ID is " + id);
-        function GetParameterValues(param) {
-        var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    var receptId = GetParameterValues('receptId'); //zet receptId door functie te draaien
+    // 
+        function GetParameterValues(param) { // functie om te bepalen welk ID er in de URL meegegeven wordt
+        var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&'); // splits na vraagteken
             console.log(url);
             for (var i = 0; i < url.length; i++) {
-            var urlparam = url[i].split('=');
-            if (urlparam[0] == param) {
-            console.log(urlparam[0]);
-            console.log(urlparam[1]);
-            return urlparam[1];
-            
-            }
+            var urlparam = url[i].split('='); // splitsen na de =
+                if (urlparam[0] == param) { // ingegeven parameter moet gelijk zijn
+                console.log(urlparam[0]); // kijken wat eerste deel is 
+                console.log(urlparam[1]); // kijken wat het 2e deel is en direct de ID
+                return urlparam[1]; // return de ID naar var receptId
+                
+                }
             }
         }
-        // console.log(" waarde van geclickte recept" + receptId);
+       
         
         /*------------------------------ 
             Json data collecton
         --------------------------------*/
-        $.getJSON("../../json/recepten.json", function(data){
+        $.getJSON("../../json/recepten.json",{
+           // zoals aangegeven door docent, is dit het ophalen van de Json. 
            
+            }).done(function(data){
+                // de rest in de done gedaan zodat hij niet meer streamed begreep ik van Harald, of verkeerd begrepen. 
             console.log(data);
             $.each(data, function(key, value){
                 if(value.id == receptId){
                     console.log("ik ben recept "+ value.title);
                     console.log(value);
-                    var id = value.id;
-                    var titel = value.title;
-                    var beschrijving = value.textComplete;
-                    var ingredienten = value.ingredienten;
-                    var receptPlaatje = value.image;
-                    var hoofdIngredient = value.hoofdIngredient;
-                    var tijd = value.tijd;
-                    console.log("in de stream " + tijd)
-                    var benodigdheden = value.benodigdheden;
+                    var v = value; // verkort van telkens value schrijven
+                    
 
                 }
-            });
-            
-        }).done(function(value){
-            console.log("in de done de tijd "+ tijd);
+           
         });
+  
         // $('.boekenCardHolder').append(boekenMarkup);
     });
+    
 
-   
+});
