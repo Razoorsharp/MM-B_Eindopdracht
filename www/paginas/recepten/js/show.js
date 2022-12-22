d92c9380578efee0219cac18f6c631bda04f54ce
PimDamen <pim.damen@wearewuunder.com>
2022-12-22 11:37:48 +0100

    working Json on show recepten
    working json on

--- www/paginas/recepten/js/show.js
+++ www/paginas/recepten/js/show.js
@@ -1,8 +1,11 @@
 var d = document;
 
 $(document).ready(function () {
-    var id = GetParameterValues('receptId');
-    alert("Hello your ID is " + id);
+    // -----------------------------
+    // Determine receptId
+    // ------------------------------
+    var receptId = GetParameterValues('receptId');
+    // alert("Hello your ID is " + id);
         function GetParameterValues(param) {
         var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
             console.log(url);
@@ -16,9 +19,22 @@ $(document).ready(function () {
             }
             }
         }
-        var receptId = Id;
-
-        $get
+        // console.log(" waarde van geclickte recept" + receptId);
+        
+        /*------------------------------ 
+            Json data collecton
+        --------------------------------*/
+        $.getJSON("../../json/recepten.json", function(data){
+           
+            console.log(data);
+            $.each(data, function(key, value){
+                if(value.id == receptId){
+                    console.log("ik ben recept "+ value.title);
+                }
+            });
+            
+        });
+        // $('.boekenCardHolder').append(boekenMarkup);
     });
 
    
