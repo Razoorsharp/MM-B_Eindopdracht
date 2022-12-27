e2b879afa690c32a8271977fb12240059e87fc82
PimDamen <pim.damen@wearewuunder.com>
2022-12-27 16:47:28 +0100

    recept grid
    lettertype is te klein en maak je hem groter dan past de content niet in het grid indanks dat ik 1fr heb gebruikt?

--- www/paginas/recepten/js/show.js
+++ www/paginas/recepten/js/show.js
@@ -46,25 +46,25 @@ $(document).ready(function () {
                     console.log("ik ben recept "+ value.title);
                     console.log(value);
                     var v = value; // verkort van telkens value schrijven
-//                     var receptMarkup = ''; // begin met maken van markup voor recept data
+                    var receptMarkup = ''; // begin met maken van markup voor recept data
 
-//                     receptMarkup += '<div class="contentSpacer"><div class="receptHeader"><h2>Ingredienten</h2> <ul>'; // maak dedivs aan als  de spacer heading en ingredienten titel, opent ook UL
-//                     receptMarkup += '<li class="receptHoofdIngr"><img src="../../images/icons/32px/'+ value.hoofdIngredient +'.png"></li>'; // vul hoofdingredient icon in
-//                     receptMarkup += ' <li class="receptGerei"><img src="../../images/icons/32px/'+ value.benodigdheden +'.png"></li>'; // vul benodigheden icon in
-//                     receptMarkup += '<li class="recentTime receptTime'+ value.tijd +'"><img src="../../images/icons/32px/163.png"></li>'; // zorg dat tijd en tijdicon goed staan
-//                     receptMarkup += '</ul></div>';// sluit bovendte deel van de heading
-//                     receptMarkup += '<div class="recepList">'// maak div met class voor ingredientenlijst
-// ;                   receptMarkup += '<ul>'; // open UL voor ingredientenlijst
-//                         $.each(value.ingredienten, function(id, ingredient){ // ga door de lijst van ingredienten en zet daar voor een LI neer in de UL
+                    receptMarkup += '<div class="contentSpacer"><div class="receptHeader"> <ul>'; // maak dedivs aan als  de spacer heading en ingredienten titel, opent ook UL
+                    receptMarkup += '<li class="receptHoofdIngr"><img src="../../images/icons/32px/'+ value.hoofdIngredient +'.png"></li>'; // vul hoofdingredient icon in
+                    receptMarkup += ' <li class="receptGerei"><img src="../../images/icons/32px/'+ value.benodigdheden +'.png"></li>'; // vul benodigheden icon in
+                    receptMarkup += '<li class="receptTime receptTime'+ value.tijd +'"><img src="../../images/icons/32px/163.png"></li>'; // zorg dat tijd en tijdicon goed staan
+                    receptMarkup += '</ul></div>';// sluit bovendte deel van de heading
+                    receptMarkup += '<div class="receptList"><h2>Ingredienten</h2>'// maak div met class voor ingredientenlijst
+;                   receptMarkup += '<ul>'; // open UL voor ingredientenlijst
+                        $.each(value.ingredienten, function(id, ingredient){ // ga door de lijst van ingredienten en zet daar voor een LI neer in de UL
                         
-//                             receptMarkup += '<li>'+ ingredient +'</li>';
-//                         });
-//                     receptMarkup += '</ul></div>';// sluit UL en bijhorende DIV
-//                     receptMarkup += '<div class="receptTekst"><h2>Bereidingswijze</h2>'; // open receptTekst div met titel bereidingswijze
-//                     receptMarkup += '<p>'+ value.tekstComplete +'</p>' // voeg complete tekst toe. 
-//                     receptMarkup += '</div></div></div>'; // sluit alle divs
+                            receptMarkup += '<li>'+ ingredient +'</li>';
+                        });
+                    receptMarkup += '</ul></div>';// sluit UL en bijhorende DIV
+                    receptMarkup += '<div class="receptTekst"><h2>Bereidingswijze</h2>'; // open receptTekst div met titel bereidingswijze
+                    receptMarkup += '<p>'+ value.tekstComplete +'</p>' // voeg complete tekst toe. 
+                    receptMarkup += '</div></div></div>'; // sluit alle divs
 
-//                     $('#receptData').append(receptMarkup);
+                    $('#receptData').append(receptMarkup);
 
                 }
                 
