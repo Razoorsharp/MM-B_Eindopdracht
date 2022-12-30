8c01f757ce70f8235feb1a1e189be21ea988fbea
PimDamen <pim.damen@wearewuunder.com>
2022-12-30 11:08:56 +0100

    .
    .

--- www/js/nav.js
+++ www/js/nav.js
@@ -10,12 +10,12 @@ $(document).ready(function(){
     navigationMarkup += '<div class="mobile-container"> <div class="topnav"> <div class="navGlyphicons"><a href="#"><span class="glyphicon glyphicon-user"></span>'
     navigationMarkup += '</a><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> </a> </div>'
     navigationMarkup += '<a href="#home" class="active"></a><div class="linkContainer" id="myLinks"><div class="spacer"></div>';
-    navigationMarkup += '<a href="#news">Campagne</a>';
-    navigationMarkup += '<a href="paginas/recepten/index.html">Recepten</a>';
+    navigationMarkup += '<a href="/paginas/campagne">Campagne</a>';
+    navigationMarkup += '<a href="/paginas/recepten/index.html">Recepten</a>';
     navigationMarkup += '<a href="#">Nieuws</a>';
     navigationMarkup += '<div class="spacer"></div>';
     navigationMarkup += '<a href="#">Over ons</a>';
-    navigationMarkup += '<a href="paginas/contact/index.html">Contact</a>';
+    navigationMarkup += '<a href="/paginas/contact">Contact</a>';
     navigationMarkup += '<a href="#">Shop</a>';
     navigationMarkup += '<div class="spacer"></div></div>';
     navigationMarkup += ' <a href="#" class="icon" id="navClick" ><i class="fa fa-bars"></i></a></div></div>';
