83b9ed0498f8e017a6b211722694819ccfd9e959
PimDamen <pim.damen@wearewuunder.com>
2023-01-11 15:02:40 +0100

    .
    .

--- www/js/nav.js
+++ www/js/nav.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
         // navigation Creator 
         // ------------------
   var navigationMarkup='';
-    navigationMarkup += '<div class="mobile-container"> <div class="topnav"> <div class="navGlyphicons"><a href="#"><span class="glyphicon glyphicon-user"></span>'
-    navigationMarkup += '</a><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> </a> </div>'
+    navigationMarkup += '<div class="mobile-container"> <div class="topnav"> <div class="navGlyphicons"><a href="/paginas/account/index.html"><span class="glyphicon glyphicon-user"></span>'
+    navigationMarkup += '</a><a href="/paginas/shop/cart/index.html"><span class="glyphicon glyphicon-shopping-cart"></span> </a> </div>'
     navigationMarkup += '<a href="#home" class="active"></a><div class="linkContainer" id="myLinks"><div class="spacer"></div>';
     navigationMarkup += '<a href="/paginas/campagne">Campagne</a>';
     navigationMarkup += '<a href="/paginas/recepten/index.html">Recepten</a>';
