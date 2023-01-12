05d1aa8b3d3ae60e09252e12d5e67799e3a41a73
PimDamen <pim.damen@wearewuunder.com>
2023-01-12 14:57:01 +0100

    fixed by marcel
    Name="was the key of the index of formObject.

--- www/paginas/account/js/gegevens.js
+++ www/paginas/account/js/gegevens.js
@@ -8,31 +8,37 @@ $(document).ready(function(){
 
     var gegevensSubmit = d.getElementById("gegevensSubmit")
     var inputNaam= d.getElementById("naam");
-    const form = document.getElementById('persoonlijkeGegevensForm');
+    // var form = document.getElementById('persoonlijkeGegevensForm');
     console.log("geladen");
-    
-    form.addEventListener('submit', callbackFunction); // draai callback functie als form submit wordt. 
-                        
-    function callbackFunction(event) {
-        console.log("draait callback")
-    event.preventDefault(); // prevent default om te zorgen dat het de form data niet direct verdwijnt. 
-    const myFormData = new FormData(event.target);
-            // *NOTE:  zodra onderstaande formDataObj weg is. doet hij het wel.. alleen is myFormData leeg.... 
-    const formDataObj = {};
-    myFormData.forEach((value, key) => (formDataObj[key] = value)); // alle values sorteren. 
-    console.log(formDataObj); // weergeven van de value en keys in een object zodat dit later doorgezet kan worden naar een Json of database met behulp van PHP of API. 
-             // *NOTE:  zodra bovenstaande formDataObj er is. doet hij het niet.. komt niets in het console zelfs geen foutmelding
-
-    };
+ 
+    // };
 
 
 
     // EVENT
     gegevensSubmit.addEventListener('click', function(e){
         console.log("submit gedrukt");
-    e.preventDefault();
-    
+        
+   
+        const form = document.getElementById('persoonlijkeGegevensForm');
 
+        form.addEventListener('submit', callbackFunction); // draai callback functie als form submit wordt. 
+                            
+        function callbackFunction(event) {
+            console.log("draait callback")
+        event.preventDefault(); // prevent default om te zorgen dat het de form data niet direct verdwijnt. 
+        const myFormData = new FormData(event.target);
+
+
+        const formDataObj = {};
+        //myFormData.forEach((value, key) => (formDataObj[key] = value)); // alle values sorteren. 
+        myFormData.forEach((value, key) => {
+            console.log(value, key);
+            localStorage.setItem(key, value);
+        });
+        console.log(formDataObj); // weergeven van de value en keys in een object zodat dit later doorgezet kan worden naar een Json of database met behulp van PHP of API. 
+
+    };
    
 
 
